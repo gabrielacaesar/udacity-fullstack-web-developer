b7ebeab51680c600ec09bc7b552172fe5fb31aa5
gabrielacaesar <gabrielacaesar55@gmail.com>
2018-12-27 22:55:44 -0200

    Add files via upload

---
+++ entertainment-27dez2018.py
@@ -0,0 +1,10 @@
+# remove the date of the title for running
+
+import media
+
+toy_story = media.Movie("Toy Story",
+						"A story of a boy...",
+						"https://www.",
+						"https://www.")
+
+print(toy_story.storyline)
